3a00f39576e7dcd85ab54418f7e378c400c9657c
Jason Koo <jkoo2088@gmail.com>
2019-11-27 15:35:27 -0500

    updated register route

--- api/routes/authUser.js
+++ api/routes/authUser.js
@@ -2,6 +2,24 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
 const Song = require('../models/song')
+const User = require('../models/user');
+
+
+router.post('/register', async (req, res) => {
+    const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+
+    }catch(err){
+        res.status(400).send(err);
+    }
+});
 
 router.get('/',(req, res, next) => {
     res.status(200).json({
