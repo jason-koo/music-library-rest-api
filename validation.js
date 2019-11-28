1ffba3fa70dd0f02d1476e641b2d6e3310690a2c
Jason Koo <jkoo2088@gmail.com>
2019-11-28 00:57:39 -0500

    created a validation file that will handle all validations

---
+++ validation.js
@@ -0,0 +1,11 @@
+// VALIDATION
+const Joi = require('@hapi/joi');
+
+const registerValidation = data => {
+    const schema = Joi.object({
+        username: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    });
+    return schema.validate(data);
+ };
