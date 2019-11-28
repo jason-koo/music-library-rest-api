8e62dbd24d287c1c331654ace50fd065edafe7b0
Jason Koo <jkoo2088@gmail.com>
2019-11-28 17:16:30 -0500

    updated song model

--- api/models/song.js
+++ api/models/song.js
@@ -4,6 +4,12 @@ const songSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     artist: String,
     name: String,
+    album: String,
+    year: Number,
+    genre: String,
+    avgRating: Number,
+    reviewNum: Number,
+    ratingNum: Number
 });
 
 module.exports = mongoose.model('Song', songSchema);
