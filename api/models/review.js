8e1a1494606e4a375f1dfd951e2882e549951bda
Jason Koo <jkoo2088@gmail.com>
2019-11-28 17:16:05 -0500

    updated review model

--- api/models/review.js
+++ api/models/review.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
+    objectID: {
+        type: Number,
+        unique: true
+    },
     rating: {
         type: Number
+    },
+    songName: {
+        type: String,
+        required: true
+    },
+    submittedBy: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    avgRating: {
+        type: Number
     }
-
 });
+
+module.exports = mongoose.model('Review', reviewSchema);
