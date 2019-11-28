e2394ab95b4c2e9f31037bd59d2fc079c50cf548
Jason Koo <jkoo2088@gmail.com>
2019-11-28 01:02:11 -0500

    added login validation

--- validation.js
+++ validation.js
@@ -1,11 +1,25 @@
 // VALIDATION
 const Joi = require('@hapi/joi');
 
+
+// Register validation
 const registerValidation = data => {
-    const schema = Joi.object({
+    const schema = {
         username: Joi.string().min(6).required(),
         email: Joi.string().min(6).required().email(),
         password: Joi.string().min(6).required()
-    });
-    return schema.validate(data);
+    };
+    return Joi.validate(data, schema);
  };
+
+ // Login validation
+ const loginValidation = data => {
+    const schema = {
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    };
+    return Joi.validate(data, schema);
+ };
+
+ module.exports.registerValidation = registerValidation;
+ module.exports.loginValidation = loginValidation;
