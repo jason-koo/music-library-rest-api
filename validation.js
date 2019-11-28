676c83a893156fbe05e74705c8855f2e1056cf5d
Jason Koo <jkoo2088@gmail.com>
2019-11-28 01:07:18 -0500

    updted validation methods

--- validation.js
+++ validation.js
@@ -4,21 +4,21 @@ const Joi = require('@hapi/joi');
 
 // Register validation
 const registerValidation = data => {
-    const schema = {
+    const schema = Joi.object({
         username: Joi.string().min(6).required(),
         email: Joi.string().min(6).required().email(),
         password: Joi.string().min(6).required()
-    };
-    return Joi.validate(data, schema);
+    });
+    return schema.validate(data);
  };
 
  // Login validation
  const loginValidation = data => {
-    const schema = {
+    const schema = Joi.object({
         email: Joi.string().min(6).required().email(),
         password: Joi.string().min(6).required()
-    };
-    return Joi.validate(data, schema);
+    });
+    return schema.validate(data);
  };
 
  module.exports.registerValidation = registerValidation;
