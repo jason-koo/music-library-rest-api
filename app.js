0dca81f87915775760eb69f019136fa6a78a75c6
Jason Koo <jkoo2088@gmail.com>
2019-12-03 00:23:22 -0500

    updated file

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use((req, res, next) => {
     next(error);
 });
 app.use((error, req, res, next) => {
-    res.status(err.status || 500);
+    res.status(error.status || 500);
     res.json({
         error: {
             message: error.message
