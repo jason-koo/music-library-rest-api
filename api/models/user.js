aba27de2aa520e6428b92fe6304fa2af5519770c
Jason Koo <jkoo2088@gmail.com>
2019-12-03 01:21:33 -0500

    updated user model

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    objectId: mongoose.Schema.Types.ObjectId,
     username: {
         type: String,
         required: true,
@@ -23,6 +24,18 @@ const userSchema = new mongoose.Schema({
     date: {
         type: Date,
         defualt: Date.now
+    },
+    confirmed: {
+        type: Boolean,
+        default: false
+    },
+    active: {
+        type: Boolean,
+        default: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
 });
 
