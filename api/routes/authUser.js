38736ebfbfab74f354ea3088b4c93f72d3afb391
Jason Koo <jkoo2088@gmail.com>
2019-12-03 01:22:42 -0500

    updated routes for authenticated users

--- api/routes/authUser.js
+++ api/routes/authUser.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
 const Song = require('../models/song')
+const Review = require('../models/review')
 
 
 const verify = require('./verifyToken');
@@ -18,35 +19,45 @@ router.put('/song',(req, res, next) => {
     });
 });
 
-router.post('/something',(req, res, next) => {
-    res.status(200).json({
-        message: 'Updates the record of the given song ID with JSON array of properties sent in the body'
-    });
-});
 
-router.post('/song/create',(req, res, next) => {
+router.post('/add-song', async (req, res) => {
+
     const song = new Song({
-        _id: new mongoose.Types.ObjectId(),
+        objectID: req.body.objectID,
+        title: req.body.title,
         artist: req.body.artist,
-        name: req.body.name
+        album: req.body.album,
+        year: req.body.year,
+        comment: req.body.comment,
+        genre: req.body.genre,
+        avgRating: req.body.avgRating,
+        numOfReviews: req.body.numOfReviews
     });
-    song
-    .save()
-    .then(result => {
-        console.log(result);
-    })
-    .catch(err => console.log(err));
-    res.status(200).json({
-        message: 'handling post',
-        createdSong: song
-    })
+    try {
+        const savedSong = await song.save();
+        res.send({song})
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 
-router.put('/add-review/:id',(req, res, next) => {
-    res.status(200).json({
-        message: 'Create a new review for the song with the given ID based on JSON array provided in the body.'
+router.post('/add-review/:objID', async (req, res) => {
+
+    const review = new Review({
+        _id: new mongoose.Types.ObjectId(),
+        objectID: req.params.objID,
+        rating: req.body.rating,
+        songName: req.body.songName,
+        submittedBy: req.body.submittedBy,
+        description: req.body.description
     });
+    try {
+        const savedReview = await review.save();
+        res.send({review})
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 module.exports = router;
