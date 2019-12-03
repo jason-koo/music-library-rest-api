dd40238ad204fa215ce0e8f581c7c4930d8e0142
Jason Koo <jkoo2088@gmail.com>
2019-12-03 01:23:19 -0500

    added new router for open users

--- api/routes/openUser.js
+++ api/routes/openUser.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Song = require('../models/song');
+const Review = require('../models/review');
 const { registerValidation, loginValidation } = require('../../validation');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+var stringSimilarity = require('string-similarity');
 
 
 // REGISTER
@@ -32,6 +35,7 @@ router.post('/register', async (req, res) => {
         email: req.body.email,
         password: hashedPassword
     });
+    
     try{
         const savedUser = await user.save();
         res.send({user: user._id});
@@ -48,13 +52,16 @@ router.post('/login', async (req, res) => {
      // CHECK IF THE USER IS ALREADY IN THE DATABASE
     const user = await User.findOne({email: req.body.email});
     if(!user) { return res.status(400).send('Email or password is incorrect'); }
+    
+    //if(!user.confirmed) { return res.status(400).send("Please confirm your email to login")}
+    if(!user.active) { return res.status(400).send("This account is deactivated, please contact admin")}
     // CHECK IF PASSWORD IS CORRECT
     const validPassword =  await bcrypt.compare(req.body.password, user.password);
     if(!validPassword){ return res.status(400).send("Invalid password")}
 
     //CREATE AND ASSIGN A TOKEN
     const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-    res.header('auth-token').send(token);
+    res.header('auth-token', token).send({token});
 
 });
 
@@ -67,30 +74,104 @@ router.get('/',(req, res, next) => {
 });
 
 
-// Returns a list of songs, top 10
+// Returns a list of songs, top 10, returns rating
 router.get('/songs',(req, res, next) => {
-    res.status(200).json({
-        message: 'Returns a list of 10 songs ordered by' +
-         'average rating. Optionally, you may pass a query' +
-         'parameter to indicate the number of the results to return.'
-    });
+    Song.find()
+    .sort({avgRating:-1}).limit(10)
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        res.send(docs);
+
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
+    
 });
+
+router.get('/allSongs', (req, res, next) => {
+    Song.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        res.send(docs);
+
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
+})
 
 
 // search for songs
-router.get('/song/search', (req, res, next) => {
-    res.status(200).json({
-        message: 'Returns a list of songs matching the search criteria provided as query parameters'
-    });
+router.get('/song/search/:value', (req, res) => {
+    var arr = []
+    var songArr = []
+    song = req.params.value
+    Song.find((err, docs) => {
+        if(err) {
+            console.log("Error" + JSON.stringify(err, undefined, 2));
+        } else {
+            arr = docs;
+            for(var i = 0; i < arr.length; i++) {
+                if(stringSimilarity.compareTwoStrings(arr[i].title, song) > 0.5) {
+                    songArr.push(arr[i]);
+                } else if(stringSimilarity.compareTwoStrings(arr[i].artist, song) > 0.5) {
+                    songArr.push(arr[i]);
+                } else if(stringSimilarity.compareTwoStrings(arr[i].album, song) > 0.5) {
+                    songArr.push(arr[i]);
+                } 
+            }
+            res.send(songArr)
+        }
+    })
 });
 
 
 // gets all the reviews for a song
-router.get('/song/reviews/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'Returns all reviews for a given song ID'
-    });
+router.get('/show-reviews/:objID', (req, res, next) => {
+    Review.find({objectID: req.params.objID})
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        res.send(docs);
+
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
+
 });
+
+router.get('/song-details/:objID', (req, res, next) => {
+    Song.find({objectID: req.params.objID})
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        res.send(docs);
+
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
+})
 
 module.exports = router;
 
