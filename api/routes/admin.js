b4f3ba55b7dbda982233d66600e41b92a3f38795
Jason Koo <jkoo2088@gmail.com>
2019-12-03 01:22:14 -0500

    added rew routes to admin for deactivation/activation

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,13 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Requires admin privileges'
-    });
+router.get('/users', (req, res, next) => {
+    User.find()
+    .sort({avgRating:-1}).limit(10)
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        res.send(docs);
+
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
 });
 
+router.post('/makeAdmin/:userID', (req, res, next) => {
+    User.update({ _id: req.params.userID }, {"$set": {"isAdmin":true}})
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        res.send(docs)
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    })
+})
+
+router.post('/deactivate/:userID', (req, res, next) => {
+    User.update({ _id: req.params.userID }, {"$set": {"active":false}})
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        res.send(docs)
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    })
+});
+
+router.post('/activate/:userID', (req, res, next) => {
+    User.update({ _id: req.params.userID }, {"$set": {"active":true}})
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        res.send(docs)
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    })
+})
 
 router.get('/copyright', (req, res, next) => {
     res.status(200).json({
@@ -23,11 +77,7 @@ router.post('/copyright/:id', (req, res, next) => {
 });
 
 
-router.post('/deactivate/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'Set or clear "account deactivated" flag for a given user'
-    });
-});
+
 
 
 
