44c386a19a776419d6903673021fd85e841d9ce6
Jason Koo <jkoo2088@gmail.com>
2019-12-03 01:23:56 -0500

    added method to verify token when logging in

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    const token = req.header('auth-token');
+    const token = req.header('authorization');
     if(!token) { return res.status(401).send('Access Denied')};
 
     try{
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         res.user = verified;
+        next();
     }catch(err) {
         res.status(400).send('Invalid Token');
     }
